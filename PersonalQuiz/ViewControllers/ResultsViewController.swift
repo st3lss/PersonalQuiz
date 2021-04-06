f22986c42ebeedfb448b3f6d64d4c9bbe8fc9cc8
st3lss <st3ls@yandex.ru>
2021-04-06 13:26:39 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 05.04.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+    
+    // Mark: Outlets & values
+    
+    @IBOutlet var resultAnimalType: UILabel!
+    @IBOutlet var resultDescription: UILabel!
+    
+    var answersChoosen: [Answer] = []
+    
+    // Mark: Private values
+    private var dogAnswers: [Answer] = []
+    private var catAnswers: [Answer] = []
+    private var rabbitAnswers: [Answer] = []
+    private var turtleAnswers: [Answer] = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createNewArrays()
+        determineWhoAreYou()
+        
+        
+    }
+    
+    
+    
+    
+}
+
+// Mark: Private methods
+extension ResultsViewController{
+    private func createNewArrays() {
+        for a in answersChoosen {
+            if a.type == PersonalQuiz.AnimalType.dog {
+                dogAnswers.append(a)
+            } else if a.type == PersonalQuiz.AnimalType.cat {
+                catAnswers.append(a)
+            } else if a.type == PersonalQuiz.AnimalType.rabbit {
+                rabbitAnswers.append(a)
+            } else {
+                turtleAnswers.append(a)
+            }
+        }
+    }
+    
+    private func determineWhoAreYou() {
+    
+        if dogAnswers.count > catAnswers.count && dogAnswers.count > rabbitAnswers.count && dogAnswers.count > turtleAnswers.count {
+            resultAnimalType.text = "Вы \(AnimalType.dog.rawValue)"
+            resultDescription.text = "\(AnimalType.dog.definition)"
+            
+        } else if catAnswers.count > dogAnswers.count && catAnswers.count > rabbitAnswers.count && catAnswers.count > turtleAnswers.count {
+            resultAnimalType.text = "Вы \(AnimalType.cat.rawValue)"
+            resultDescription.text = "\(AnimalType.cat.definition)"
+            
+        } else if rabbitAnswers.count > dogAnswers.count && rabbitAnswers.count > catAnswers.count && rabbitAnswers.count > turtleAnswers.count {
+            resultAnimalType.text = "Вы \(AnimalType.rabbit.rawValue)"
+            resultDescription.text = "\(AnimalType.rabbit.definition)"
+        } else if turtleAnswers.count > dogAnswers.count && turtleAnswers.count > catAnswers.count && turtleAnswers.count > rabbitAnswers.count {
+            resultAnimalType.text = "Вы \(AnimalType.turtle.rawValue)"
+            resultDescription.text = "\(AnimalType.turtle.definition)"
+        } else {
+            resultAnimalType.text = ""
+            resultDescription.text = "Вы многогранная личность"
+            
+        }
+        
+    }
+    
+    
+}
